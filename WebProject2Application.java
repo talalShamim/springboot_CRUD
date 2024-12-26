f703e9d1ac52b302902a43d3751007e93a1cd29c
Talal Shamim <153133752+talalShamim@users.noreply.github.com>
2024-12-26 16:13:01 +0500

    Add files via upload

---
+++ WebProject2Application.java
@@ -0,0 +1,13 @@
+package com.codemyth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProject2Application.class, args);
+	}
+
+}
